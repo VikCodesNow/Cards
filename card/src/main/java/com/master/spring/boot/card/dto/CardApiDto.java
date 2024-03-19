a79f2ec74f0771a17e103e5e021a0ad2ae1a8177
Vik <vikram.projects.33@gmail.com>
2024-03-19 07:23:47 +0530

    Enabled config refresh using bus refresh

--- card/src/main/java/com/master/spring/boot/card/dto/CardApiDto.java
+++ card/src/main/java/com/master/spring/boot/card/dto/CardApiDto.java
@@ -1,8 +1,14 @@
 package com.master.spring.boot.card.dto;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardApiDto(String env,String version) {
+public class CardApiDto {
+    String env;
+    String version;
 }
